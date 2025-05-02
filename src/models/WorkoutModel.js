ea0dfa0a38f035ca23ca024050c6d8bbdcfddffb
Natalia Sokolova <nataliya.a.sokolova@gmail.com>
2025-05-02 16:00:35 -0700

    add workouts

--- src/models/WorkoutModel.js
+++ src/models/WorkoutModel.js
@@ -41,20 +41,20 @@ const WorkoutSchema = new mongoose.Schema({
     },
 
     weight: {
-        from: { 
-          type: Number, 
-          required: true,
-          min: 30,
-          max: 200,
-        },
-        to: { 
-          type: Number, 
-          required: true,
-          min: 30,
-          max: 200,
-        },        
-
+      from: { 
+        type: Number, 
+        required: true,
+        min: 30,
+        max: 300,  // Increased from 60 to 300
+      },
+      to: { 
+        type: Number, 
+        required: true,
+        min: 30,
+        max: 300,  // Increased from 300 to match 'from'
+      },  
     },
+            
     level: {
         type: String, 
         enum: ["Beginner","Intermediate", "Advanced"], 
